26a361542318d6d20bf0c053329fd617134245c7
Mulyono <mulyonomuiz63@gmail.com>
2026-02-04 21:36:28 +0700

    membenahi ukuran gambar

--- app/Views/partials/header.php
+++ app/Views/partials/header.php
@@ -66,14 +66,18 @@ $initials = (count($words) >= 2)
                 <!--begin::User menu-->
                 <div class="app-navbar-item ms-1 ms-md-4" id="kt_header_user_menu_toggle"> <!--begin::Menu wrapper-->
                     <div class="cursor-pointer symbol symbol-35px symbol-label fs-3 fw-bold bg-light-primary text-primary" data-kt-menu-trigger="{default: 'click', lg: 'hover'}" data-kt-menu-attach="parent" data-kt-menu-placement="bottom-end">
-                        <?= img_lazy('uploads/app-icon/' . setting('app_icon'), setting('app_name'), ['class'  => 'h-30px']) ?>
+                        <div class="symbol-label fs-3 fw-bold bg-light-primary text-primary">
+                            <?= strtoupper($initials) ?>
+                        </div>
                     </div>
                     <!--begin::User account menu-->
                     <div class="menu menu-sub menu-sub-dropdown menu-column menu-rounded menu-gray-800 menu-state-bg menu-state-color fw-semibold py-4 fs-6 w-275px" data-kt-menu="true"> <!--begin::Menu item-->
                         <div class="menu-item px-3">
                             <div class="menu-content d-flex align-items-center px-3"> <!--begin::Avatar-->
                                 <div class="symbol symbol-50px me-5 bg-light-primary text-primary">
-                                    <?= strtoupper($initials) ?>
+                                    <div class="symbol-label fs-3 fw-bold bg-light-primary text-primary">
+                                        <?= strtoupper($initials) ?>
+                                    </div>
                                 </div> <!--end::Avatar--> <!--begin::Username-->
                                 <div class="d-flex flex-column">
                                     <div class="fw-bold d-flex align-items-center fs-5"><?= get_pegawai(session()->get('user_id'))['nama_anggota'] ?><span class="badge badge-light-success fw-bold fs-8 px-2 py-1 ms-2"><?= get_pegawai(session()->get('user_id'))['status'] ?></span></div> <a href="#" class="fw-semibold text-muted text-hover-primary fs-7"><?= session()->get('email') ?></a>
